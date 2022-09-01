42bef72dc823534a8358d5d4197d1711d0fb28d0
rudans987 <rudans987@naver.com>
2022-09-01 17:27:37 +0900

    [FEAT] 상세페이지 구현 #34

--- src/components/main/postDetail/PostDetailContainer.jsx
+++ src/components/main/postDetail/PostDetailContainer.jsx
@@ -1,9 +1,51 @@
-const PostDeailContainer= ()=> {
-  
+import styled from "styled-components";
+import PostDeailUser from "./PostDetailUser";
+
+
+const PostDeailContainer= ({postDetail, setDetailopen})=> {
+  const closeContainer=()=> {
+    setDetailopen(false)
+  }
     return(
-        <>
-        </>
+        <StDeailContainer>
+       <button  className="closeButton" onClick={closeContainer}>x</button>
+        <img className="postImg" src={postDetail.postImgs[0]} alt="img"/>
+      <PostDeailUser postDetail={postDetail}/>
+      
+        <StContentBox>{postDetail.content}</StContentBox>
+        </StDeailContainer>
+
     )
 }
 
 export default PostDeailContainer;
+
+const StDeailContainer=styled.div`
+    margin-top: 20px;
+   width: 500%;
+    height: 1200px;
+    border: 1px solid;
+    border-radius: 20px;
+    padding: 20px 50px;
+
+    .closeButton {
+        float: right;
+        background-color: #fff;
+        border:none;
+        margin-top: 20px;
+        margin-right: 20px;
+        font-size: 18px;
+    width: 20px;
+    height: 20px;
+    cursor: pointer;
+    }
+    .postImg{
+        margin-top: 20px;   
+        width:100%;
+    } 
+`
+
+const StContentBox=styled.div`
+margin-top:20px;
+width:100%;
+`
--- src/components/main/postDetail/PostDetailLike.jsx
+++ src/components/main/postDetail/PostDetailLike.jsx
@@ -1,7 +1,19 @@
-const PostDeailLike= ()=> {
-  
+import React, { useState} from "react";
+
+import HeartButton from "../HeartButton"
+
+
+const PostDeailLike= ({postDetail})=> {
+    const [like, setLike] = useState(false)
+    const toggleLike = async (e) => {
+        //   const res = await axios.post(...) // [POST] 사용자가 좋아요를 누름 -> DB 갱신
+          setLike(!like)
+        }
     return(
         <>
+        <div>{postDetail.heartcount}</div>
+        <HeartButton className="likebutton" like={like} onClick={toggleLike}/>
+
         </>
     )
 }
