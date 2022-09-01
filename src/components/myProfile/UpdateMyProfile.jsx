00ef6d626b45dc20340e2ffc84fc2875f147777a
YooJinRa <rarastudy@naver.com>
2022-09-02 04:22:19 +0900

    [FEAT] 마이페이지 유저 프로필 수정모달 연결 및 내 게시글 보기(#39)

--- src/components/myProfile/UpdateMyProfile.jsx
+++ src/components/myProfile/UpdateMyProfile.jsx
@@ -1,21 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "../../elements/Button";
+import Input from "../../elements/Input";
+import Potal from "../../global/globalModal/Potal";
+import Modal from "../../global/globalModal/Modal";
 import styled from "styled-components";
 
-const UpdateMyProfile = () => {
+const UpdateMyProfile = (props) => {
+
+  // ::: 프로필 편집 모달(createPotal) 컨트롤 하기
+  const [modalOn, setModalOn] = useState(false);
+  const handleModal = () => {
+    setModalOn(!modalOn);
+  };
+
+  // ::: 유저 정보 샘플
+  const userInfo = {
+    userName: "yoojin",
+    userProfileImage: "https://avatars.githubusercontent.com/u/99028253?s=400&u=678da99d93c1eab91489f73b080993fb689c56b4&v=4",
+    userComment: "나는 개발을 사랑한다.",
+  }
+
+  
+
 
   return(
     <StUpdateMyProfileWrap>
       <StMyProfileBox>
         <p>
-          <img src="https://avatars.githubusercontent.com/u/99028253?s=400&u=678da99d93c1eab91489f73b080993fb689c56b4&v=4" alt="유저 프로필 이미지" />
+          <img src={userInfo.userProfileImage} alt={userInfo.userName} />
         </p>
         <dl>
-          <dt>닉네임</dt>
-          <dd>한 줄 소개</dd>
+          <dt>{userInfo.userName}</dt>
+          <dd>{userInfo.userComment}</dd>
         </dl>
       </StMyProfileBox>
-      <Button size="square" variant="lineSquare">프로필 편집</Button>
+      <Button
+        onClick={handleModal}
+        size="square" 
+        variant="lineSquare"
+      >
+        프로필 편집
+      </Button>
+
+      <Potal>
+        {modalOn && 
+          <Modal onClose={handleModal}>
+            <div>프로필 수정하기 폼이 들어가야 합니다~~!</div>
+            <label>프로필 이미지</label>
+            <p>
+              <img src={userInfo.userProfileImage} alt={userInfo.userName} />
+            </p>
+            <label>닉네임</label>
+            <Input 
+              size="large"
+              variant="default"
+              placeholder={userInfo.userName}
+            />
+            <label>한 줄 소개</label>
+            <Input 
+              size="large"
+              variant="default"
+              placeholder={userInfo.userComment}
+            />
+          </Modal>
+        }
+      </Potal>
     </StUpdateMyProfileWrap>
   );
 };
