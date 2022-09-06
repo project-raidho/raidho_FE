fba9e8db8319a72c19f70c3d5d7f45856533d849
YooJinRa <rarastudy@naver.com>
2022-09-07 04:00:34 +0900

    [DOCS] Readme 9월 6일 개발타임라인 작성

--- src/components/login/KakaoLogin.jsx
+++ src/components/login/KakaoLogin.jsx
@@ -1,34 +1,32 @@
 import axios from "axios";
 import { useEffect } from "react";
 import { useLocation,
-     useNavigate 
-    } from "react-router-dom";
+    useNavigate 
+  } from "react-router-dom";
 
 const KakaoLogin =  () => {
-    const location =useLocation();
-    const navigate=useNavigate();
+  const location =useLocation();
+  const navigate=useNavigate();
 
-    const kakaoLoginAction = async () => {
-        try{
-            const KAKAO_CODE = location.search.split('=')[1];
-            console.log(KAKAO_CODE)
-            //const URI = process.env.REACT_APP_BASE_URI;
-            const res= await axios.get(`http://3.34.194.84:8080/login/oauth2/code/kakao?code=${KAKAO_CODE}`);
-            console.log(res);
-            localStorage.setItem("Authorization", res.headers.authorization);
-            navigate("/");
-        } catch(error) {
-            console.log(error);
-        }
+  const kakaoLoginAction = async () => {
 
-        
-    }
-    
-    
-    useEffect(()=> {
-        kakaoLoginAction();
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [])
-    }
+    try{
+      const KAKAO_CODE = location.search.split('=')[1];
+      //const URI = process.env.REACT_APP_BASE_URI;
+      const res= await axios.get(`http://3.34.194.84:8080/login/oauth2/code/kakao?code=${KAKAO_CODE}`);
+      console.log(res);
+      localStorage.setItem("Authorization", res.headers.authorization);
+      navigate("/");
+
+    } catch(error) {
+      console.log(error);
+
+    }        
+  }
+  useEffect(()=> {
+      kakaoLoginAction();
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+}
     
 export default KakaoLogin;
