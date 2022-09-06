8547ead863286eb4c64eff474be5a07859567280
YooJinRa <rarastudy@naver.com>
2022-09-07 02:04:31 +0900

    [SETTING] env 환경변수 설정

--- src/shared/SocialOAuth.jsx
+++ src/shared/SocialOAuth.jsx
@@ -1,6 +1,5 @@
-// const KAKAO_CLIENT_ID = process.env.REACT_APP_KAKAO_CLIENT_ID
-// const KAKAO_REDIRECT_URI = process.env.REACT_APP_KAKAO_REDIRECT_URI
-
+const KAKAO_CLIENT_ID = process.env.KAKAO_CLIENT_ID
+const KAKAO_REDIRECT_URI = process.env.REACT_APP_KAKAO_REDIRECT_URI
 
 const NAVER_CLIENT_ID = process.env.REACT_APP_NAVER_CLIENT_ID
 const NAVER_REDIRECT_URI = process.env.REACT_APP_NAVER_CALLBACK_URL
@@ -24,6 +23,6 @@ const randomString = () => {
 };
 const newState = randomString();
 
-export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${secrets.KAKAO_CLIENT_ID}&redirect_uri=${secrets.KAKAO_REDIRECT_URI}&response_type=code`;
+export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_CLIENT_ID}&redirect_uri=${KAKAO_REDIRECT_URI}&response_type=code`;
 export const GOOGLE_AUTH_URL = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${GOOGLE_CLIENT_ID}&redirect_uri=${GOOGLE_REDIRECT_URI}&response_type=code&scope=email%20profile%20openid&access_type=offline`;
 export const NAVER_AUTH_URL = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${NAVER_CLIENT_ID}&redirect_uri=${NAVER_REDIRECT_URI}&state=${newState}`;
