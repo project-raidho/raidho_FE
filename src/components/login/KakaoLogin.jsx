da195b13b423bd0b7c9e1beb9ae61c660a65c312
YooJinRa <rarastudy@naver.com>
2022-09-07 01:15:40 +0900

    [FEAT] 로그인 연결

--- src/components/login/KakaoLogin.jsx
+++ src/components/login/KakaoLogin.jsx
@@ -7,17 +7,26 @@ import { useLocation,
 const KakaoLogin =  () => {
     const location =useLocation();
     const navigate=useNavigate();
+
+    const kakaoLoginAction = async () => {
+        try{
+            const KAKAO_CODE = location.search.split('=')[1];
+            console.log(KAKAO_CODE)
+            //const URI = process.env.REACT_APP_BASE_URI;
+            const res= await axios.get(`http://3.34.194.84:8080/login/oauth2/code/kakao?code=${KAKAO_CODE}`);
+            console.log(res);
+            localStorage.setItem("Authorization", res.headers.authorization);
+            navigate("/");
+        } catch(error) {
+            console.log(error);
+        }
+
+        
+    }
     
     
     useEffect(()=> {
-
-        const KAKAO_CODE = location.search.split('=')[1];
-        console.log(KAKAO_CODE)
-        //const URI = process.env.REACT_APP_BASE_URI;
-        const res= axios.get(`http://3.34.194.84/login/oauth2/code/kakao?code=${KAKAO_CODE}`);
-        console.log(res);
-        localStorage.setItem("Authorization", res.headers.authorization);
-        navigate("/");
+        kakaoLoginAction();
         
     }, [])
     }
