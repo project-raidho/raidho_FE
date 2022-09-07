2696729a2c5e79845879b17a1792dcb11d7f8660
rudans987 <rudans987@naver.com>
2022-09-07 10:31:49 +0900

    [FIX] 카카오 로그인 async awit 사용

--- src/components/login/KakaoLogin.jsx
+++ src/components/login/KakaoLogin.jsx
@@ -8,17 +8,20 @@ const KakaoLogin =  () => {
     const location =useLocation();
     const navigate=useNavigate();
     
-    
-    useEffect(()=> {
-
+    const kakaoLoginAction= async()=> {
         const KAKAO_CODE = location.search.split('=')[1];
         console.log(KAKAO_CODE)
         //const URI = process.env.REACT_APP_BASE_URI;
-        const res= axios.get(`http://3.34.194.84/login/oauth2/code/kakao?code=${KAKAO_CODE}`);
-        console.log(res);
-        localStorage.setItem("Authorization", res.headers.authorization);
+        const res= await axios.get(`http://3.34.194.84:8080/login/oauth2/code/kakao?code=${KAKAO_CODE}`);
+        console.log(res);       
+        // localStorage.setItem("Authorizatison", res.headers.authorization);
         navigate("/");
-        
+    }
+
+
+    useEffect(()=> {
+        kakaoLoginAction();
+           // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
     }
     
