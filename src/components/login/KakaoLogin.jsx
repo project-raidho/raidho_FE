7271fae5180fcf8f411ca3cb5e2c5961a74f66dc
rudans987 <rudans987@naver.com>
2022-09-12 17:23:24 +0900

    [FEAT] 메인페이지 베스트, 실시간 가져오는 함수 변경

--- src/components/login/KakaoLogin.jsx
+++ src/components/login/KakaoLogin.jsx
@@ -12,7 +12,7 @@ const KakaoLogin = () => {
       console.log(KAKAO_CODE);
       const URI = process.env.REACT_APP_BASE_URI;
       const res = await axios.get(
-        `http://15.164.166.87:8080/login/oauth2/code/kakao?code=${KAKAO_CODE}`
+        `${URI}/login/oauth2/code/kakao?code=${KAKAO_CODE}`
       );
       console.log(res);
       localStorage.setItem("Authorization", res.headers.authorization);
--- src/components/main/MainMenu.jsx
+++ src/components/main/MainMenu.jsx
@@ -1,36 +1,250 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
-
+import axios from "axios";
 
 import MainPostList from "./MainPostList";
 import Button from "../../elements/Button";
 
 const MainMenu = () => {
-    const [best, setBest] = useState(true);
-    const besthandler = () => {
-        setBest(true)
-    }
-    const latesthandler = () => {
-        setBest(false)
-    }
-    return (
-        <>
-            <StMenuset>
-                <Button
-                    size="medium"
-                    variant={best ? "primary" : "gray"}
-                    onClick={besthandler}
-                >BEST</Button>
-                <Button
-                    size="medium"
-                    variant={best ? "gray" : "primary"}
-                    onClick={latesthandler}
-                >실시간</Button>
-            </StMenuset>
-            <MainPostList best={best}/>
-        </>
-    )
-}
+  const URI = process.env.REACT_APP_BASE_URI;
+  const [best, setBest] = useState(true);
+  const [postList, setPostList] = useState([
+    {
+      id: 1,
+      imgurl:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSsYeU_xa0PN9zgmYzlkTMVGKJ4ulAGevTa9A&usqp=CAU",
+      memberImage:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSInE9w_wk9gHxSLJ44RL2NoVHnIDjXNPEgbw&usqp=CAU",
+      memberName: "김경문",
+    },
+    {
+      id: 2,
+      imgurl:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTUyJZAKnMKvY9z06Qm1jUAK3THBjrHqwaww&usqp=CAU",
+      memberImage:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSInE9w_wk9gHxSLJ44RL2NoVHnIDjXNPEgbw&usqp=CAU",
+      memberName: "김경문",
+    },
+    {
+      id: 3,
+      imgurl:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnq-U57cbWEIFs5g7iIf1t6CzLXQC1JL2s3g&usqp=CAU",
+      memberImage:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSInE9w_wk9gHxSLJ44RL2NoVHnIDjXNPEgbw&usqp=CAU",
+      memberName: "김경문",
+    },
+    {
+      id: 4,
+      imgurl:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSInE9w_wk9gHxSLJ44RL2NoVHnIDjXNPEgbw&usqp=CAU",
+      memberImage:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSInE9w_wk9gHxSLJ44RL2NoVHnIDjXNPEgbw&usqp=CAU",
+      memberName: "김경문",
+    },
+    {
+      id: 5,
+      imgurl:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRC2knmb8t5SUvbiz2Y0hVD1aqSXzcqeb1Y0g&usqp=CAU",
+      memberImage:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSInE9w_wk9gHxSLJ44RL2NoVHnIDjXNPEgbw&usqp=CAU",
+      memberName: "김경문",
+    },
+    {
+      id: 6,
+      imgurl:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSsYeU_xa0PN9zgmYzlkTMVGKJ4ulAGevTa9A&usqp=CAU",
+      memberImage:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSInE9w_wk9gHxSLJ44RL2NoVHnIDjXNPEgbw&usqp=CAU",
+      memberName: "김경문",
+    },
+    {
+      id: 7,
+      imgurl:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTUyJZAKnMKvY9z06Qm1jUAK3THBjrHqwaww&usqp=CAU",
+      memberImage:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSInE9w_wk9gHxSLJ44RL2NoVHnIDjXNPEgbw&usqp=CAU",
+      memberName: "김경문",
+    },
+    {
+      id: 8,
+      imgurl:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnq-U57cbWEIFs5g7iIf1t6CzLXQC1JL2s3g&usqp=CAU",
+      memberImage:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSInE9w_wk9gHxSLJ44RL2NoVHnIDjXNPEgbw&usqp=CAU",
+      memberName: "김경문",
+    },
+    {
+      id: 9,
+      imgurl:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSInE9w_wk9gHxSLJ44RL2NoVHnIDjXNPEgbw&usqp=CAU",
+      memberImage:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSInE9w_wk9gHxSLJ44RL2NoVHnIDjXNPEgbw&usqp=CAU",
+      memberName: "김경문",
+    },
+    {
+      id: 10,
+      imgurl:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRC2knmb8t5SUvbiz2Y0hVD1aqSXzcqeb1Y0g&usqp=CAU",
+      memberImage:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSInE9w_wk9gHxSLJ44RL2NoVHnIDjXNPEgbw&usqp=CAU",
+      memberName: "김경문",
+    },
+    {
+      id: 11,
+      imgurl:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSsYeU_xa0PN9zgmYzlkTMVGKJ4ulAGevTa9A&usqp=CAU",
+      memberImage:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSInE9w_wk9gHxSLJ44RL2NoVHnIDjXNPEgbw&usqp=CAU",
+      memberName: "김경문",
+    },
+    {
+      id: 12,
+      imgurl:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTUyJZAKnMKvY9z06Qm1jUAK3THBjrHqwaww&usqp=CAU",
+      memberImage:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSInE9w_wk9gHxSLJ44RL2NoVHnIDjXNPEgbw&usqp=CAU",
+      memberName: "김경문",
+    },
+    {
+      id: 13,
+      imgurl:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnq-U57cbWEIFs5g7iIf1t6CzLXQC1JL2s3g&usqp=CAU",
+      memberImage:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSInE9w_wk9gHxSLJ44RL2NoVHnIDjXNPEgbw&usqp=CAU",
+      memberName: "김경문",
+    },
+    {
+      id: 14,
+      imgurl:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSInE9w_wk9gHxSLJ44RL2NoVHnIDjXNPEgbw&usqp=CAU",
+      memberImage:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSInE9w_wk9gHxSLJ44RL2NoVHnIDjXNPEgbw&usqp=CAU",
+      memberName: "김경문",
+    },
+    {
+      id: 15,
+      imgurl:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRC2knmb8t5SUvbiz2Y0hVD1aqSXzcqeb1Y0g&usqp=CAU",
+      memberImage:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSInE9w_wk9gHxSLJ44RL2NoVHnIDjXNPEgbw&usqp=CAU",
+      memberName: "김경문",
+    },
+    {
+      id: 16,
+      imgurl:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSsYeU_xa0PN9zgmYzlkTMVGKJ4ulAGevTa9A&usqp=CAU",
+      memberImage:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSInE9w_wk9gHxSLJ44RL2NoVHnIDjXNPEgbw&usqp=CAU",
+      memberName: "김경문",
+    },
+    {
+      id: 17,
+      imgurl:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTUyJZAKnMKvY9z06Qm1jUAK3THBjrHqwaww&usqp=CAU",
+      memberImage:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSInE9w_wk9gHxSLJ44RL2NoVHnIDjXNPEgbw&usqp=CAU",
+      memberName: "김경문",
+    },
+    {
+      id: 18,
+      imgurl:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnq-U57cbWEIFs5g7iIf1t6CzLXQC1JL2s3g&usqp=CAU",
+      memberImage:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSInE9w_wk9gHxSLJ44RL2NoVHnIDjXNPEgbw&usqp=CAU",
+      memberName: "김경문",
+    },
+    {
+      id: 19,
+      imgurl:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSInE9w_wk9gHxSLJ44RL2NoVHnIDjXNPEgbw&usqp=CAU",
+      memberImage:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSInE9w_wk9gHxSLJ44RL2NoVHnIDjXNPEgbw&usqp=CAU",
+      memberName: "김경문",
+    },
+    {
+      id: 20,
+      imgurl:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRC2knmb8t5SUvbiz2Y0hVD1aqSXzcqeb1Y0g&usqp=CAU",
+      memberImage:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSInE9w_wk9gHxSLJ44RL2NoVHnIDjXNPEgbw&usqp=CAU",
+      memberName: "김경문",
+    },
+    {
+      id: 21,
+      imgurl:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSsYeU_xa0PN9zgmYzlkTMVGKJ4ulAGevTa9A&usqp=CAU",
+      memberImage:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSInE9w_wk9gHxSLJ44RL2NoVHnIDjXNPEgbw&usqp=CAU",
+      memberName: "김경문",
+    },
+    {
+      id: 22,
+      imgurl:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTUyJZAKnMKvY9z06Qm1jUAK3THBjrHqwaww&usqp=CAU",
+      memberImage:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSInE9w_wk9gHxSLJ44RL2NoVHnIDjXNPEgbw&usqp=CAU",
+      memberName: "김경문",
+    },
+    {
+      id: 23,
+      imgurl:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnq-U57cbWEIFs5g7iIf1t6CzLXQC1JL2s3g&usqp=CAU",
+      memberImage:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSInE9w_wk9gHxSLJ44RL2NoVHnIDjXNPEgbw&usqp=CAU",
+      memberName: "김경문",
+    },
+    {
+      id: 24,
+      imgurl:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSInE9w_wk9gHxSLJ44RL2NoVHnIDjXNPEgbw&usqp=CAU",
+      memberImage:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSInE9w_wk9gHxSLJ44RL2NoVHnIDjXNPEgbw&usqp=CAU",
+      memberName: "김경문",
+    },
+  ]);
+  const getBestPosts = async () => {
+    const res = await axios.get(`${URI}/api/post/likelist`);
+    return setPostList(res.data);
+  };
+  const getLatestPosts = async () => {
+    const res = await axios.get(`${URI}/api/post/latest`);
+    return setPostList(res.data);
+  };
+  useEffect(() => {
+    getBestPosts();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const besthandler = () => {
+    getBestPosts();
+    setBest(true);
+  };
+  const latesthandler = () => {
+    getLatestPosts();
+    setBest(false);
+  };
+  return (
+    <>
+      <StMenuset>
+        <Button
+          size="medium"
+          variant={best ? "primary" : "gray"}
+          onClick={besthandler}
+        >
+          BEST
+        </Button>
+        <Button
+          size="medium"
+          variant={best ? "gray" : "primary"}
+          onClick={latesthandler}
+        >
+          실시간
+        </Button>
+      </StMenuset>
+      <MainPostList postList={postList} />
+    </>
+  );
+};
 
 export default MainMenu;
 const StMenuset = styled.div`
