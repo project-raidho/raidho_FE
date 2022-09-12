476099f3c0ce1616aa575b19f9b9d19b5107c8e1
rudans987 <rudans987@naver.com>
2022-09-12 17:22:35 +0900

    [STYLE] 모집방 개설 페이지 스타일 변경

--- src/components/createMeeting/CreateMeetingContainer.jsx
+++ src/components/createMeeting/CreateMeetingContainer.jsx
@@ -1,181 +1,174 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import styled from "styled-components";
-import ThemeSelect from './ThemeSelect';
+import ThemeSelect from "./ThemeSelect";
 
-import axios from 'axios';
-import Button from '../../elements/Button';
-import TripPeriod from './TripPeriod';
+import axios from "axios";
+import Button from "../../elements/Button";
+import TripPeriod from "./TripPeriod";
 
-import CreatePostTags from '../createPost/CreatePostTags';
-import RoomCloseDateBox from './RoomclosedateBox';
-import MeetingLocationSearch from './MeetingLocationSearch';
-import TripPeopleCount from './TripPeopleCount';
-import TextField from '@mui/material/TextField';
+import CreatePostTags from "../createPost/CreatePostTags";
+import RoomCloseDateBox from "./RoomclosedateBox";
+import MeetingLocationSearch from "./MeetingLocationSearch";
+import TripPeopleCount from "./TripPeopleCount";
+import TextField from "@mui/material/TextField";
 
 const CreateMeetingContatiner = () => {
-    const [theme, setTheme] = useState("");
-    const [locationtags, setLocationTags] = useState([]);
-    const [title, setTitle] = useState("");
-    const [desc, setDesc] = useState("");
-    const [tags, setTags] = useState([]);
-    const [people, setPeople] = useState();
-    const [roomClosedate, setRoomCloseDate] = useState()
-    const [tripPeriod, setTripPeriod] = useState([{ startDate: "", endDate: "" }]);
-    const [departLocation, setDepartLocation] = useState();
+  const [theme, setTheme] = useState("");
+  const [locationtags, setLocationTags] = useState([]);
+  const [title, setTitle] = useState("");
+  const [desc, setDesc] = useState("");
+  const [tags, setTags] = useState([]);
+  const [people, setPeople] = useState();
+  const [roomClosedate, setRoomCloseDate] = useState();
+  const [tripPeriod, setTripPeriod] = useState([
+    { startDate: "", endDate: "" },
+  ]);
+  const [departLocation, setDepartLocation] = useState();
 
-    const selectedLocationTags = (tags) => {
-        setLocationTags(tags);
-    };
+  const selectedLocationTags = (tags) => {
+    setLocationTags(tags);
+  };
 
+  const selectedTags = (tags) => {
+    setTags(tags);
+  };
 
-    const selectedTags = (tags) => {
-        setTags(tags);
-    };
+  const URI = process.env.REACT_APP_BASE_URI;
+  // const accesstoken = localStorage.getItem("Authorization");
+  // const refreshtoken = localStorage.getItem("RefreshToken");
 
+  // let config = {
+  //     headers: {
+  //         Authorization: accesstoken,
+  //         RefreshToken: refreshtoken,
+  //     },
+  // };
+  const data = {
+    theme: theme,
+    locationtags: locationtags,
+    title: title,
+    desc: desc,
+    tripPeriod: tripPeriod,
+    tags: tags,
+    people: people,
+    roomClosedate: roomClosedate,
+    departLocation: departLocation,
+  };
+  console.log(data);
+  const postcreatemeeting = async (id) => {
+    const res = await axios.post(
+      `${URI}/detail/${id}`,
+      data
+      // config
+    );
+    return res;
+  };
 
+  return (
+    <StContainer>
+      <p>step 1. 여행정보 입력</p>
+      <h1>대륙 입력</h1>
+      <ThemeSelect setTheme={setTheme} />
 
+      <h1>여행갈 나라/도시 입력</h1>
+      <StTags>
+        <CreatePostTags
+          className="tagbox"
+          selectedTags={selectedLocationTags}
+          tags={["예시)프랑스"]}
+          tagMassage={"여행할 도시나 나라를 입력해주세요!"}
+        />
+      </StTags>
+      <h1>여행기간</h1>
+      <TripPeriod setTripPeriod={setTripPeriod} />
+      <h1>여행희망인원</h1>
+      <TripPeopleCount setPeople={setPeople} />
 
+      <p>step 2. 모집글정보 입력</p>
+      <h1>모집글 제목</h1>
+      <StTitleBox
+        multiline
+        maxRows={4}
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <h1>모집글 설명</h1>
+      <StDescBox
+        id="outlined-multiline-static"
+        multiline
+        rows={4}
+        value={desc}
+        onChange={(e) => setDesc(e.target.value)}
+      />
 
-    const URI = process.env.REACT_APP_BASE_URI;
-    // const accesstoken = localStorage.getItem("Authorization");
-    // const refreshtoken = localStorage.getItem("RefreshToken");
+      <h1>해시태그</h1>
+      <StTags>
+        <CreatePostTags
+          className="tagbox"
+          selectedTags={selectedTags}
+          tags={["예시)활동적"]}
+          tagMassage={"홍보하고 싶은 내용을 자유롭게 입력해주세요!"}
+        />
+      </StTags>
 
-    // let config = {
-    //     headers: {
-    //         Authorization: accesstoken,
-    //         RefreshToken: refreshtoken,
-    //     },
-    // };
-    const data = {
-        theme: theme,
-        locationtags: locationtags,
-        title: title,
-        desc: desc,
-        tripPeriod: tripPeriod,
-        tags: tags,
-        people: people,
-        roomClosedate:roomClosedate,
-        departLocation: departLocation
-    }
-    console.log(data);
-    const postcreatemeeting = async (id) => {
-        const res = await axios.post(`${URI}/detail/${id}`,
-            data,
-            // config
-        );
-        return res;
-    };
+      <h1>모집마감일자</h1>
+      <RoomCloseDateBox setRoomCloseDate={setRoomCloseDate} />
 
-    return (
-        <StContainer>
+      <h1>모집 후 모일 장소</h1>
+      <MeetingLocationSearch setDepartLocation={setDepartLocation} />
 
-            <h1>테마</h1>
-            <ThemeSelect setTheme={setTheme} />
-
-            <h1>여행장소</h1>
-            <StTags>
-                <CreatePostTags
-                    className="tagbox"
-                    selectedTags={selectedLocationTags}
-                    tags={['예시)프랑스']}
-                    tagMassage={'여행할 도시나 나라를 입력해주세요!'}
-                />
-
-            </StTags>
-            <h1>제목</h1>
-            <StTitleBox
-                multiline
-                maxRows={4}
-               
-                value={title}
-                onChange={(e) => setTitle(e.target.value)}
-            />
-            <h1>설명</h1>
-            <StDescBox
-                id="outlined-multiline-static"
-                multiline
-                rows={4}
-              
-                value={desc}
-                onChange={(e) => setDesc(e.target.value)}
-            />
-
-
-            <h1>해시태그</h1>
-            <StTags>
-                <CreatePostTags
-                    className="tagbox"
-                    selectedTags={selectedTags}
-                    tags={['예시)활동적']}
-                    tagMassage={'태그를 입력해주세요!'}
-                />
-
-            </StTags>
-
-
-            <h1>모집인원</h1>
-            <TripPeopleCount setPeople={setPeople} />
-            <h1>모집마감일자</h1>
-            <RoomCloseDateBox setRoomCloseDate={setRoomCloseDate}/>
-            <h1>여행기간</h1>
-            <TripPeriod setTripPeriod={setTripPeriod} />
-            <h1>출발장소</h1>
-            <MeetingLocationSearch setDepartLocation={setDepartLocation} />
-
-            <Button
-                size="small"
-                variant="primary"
-                onClick={postcreatemeeting}
-            >개설하기</Button>
-        </StContainer>
-
-    )
-}
-export default CreateMeetingContatiner
+      <Button size="small" variant="primary" onClick={postcreatemeeting}>
+        등록하기
+      </Button>
+    </StContainer>
+  );
+};
+export default CreateMeetingContatiner;
 
 const StContainer = styled.div`
-    h1{
-        font-size:20px;
-        margin-top: 20px;
-    }
-`
+  width: 50%;
+  p {
+    font-size: 30px;
+  }
+  h1 {
+    font-size: 20px;
+    margin-top: 20px;
+  }
+`;
 const StTitleBox = styled(TextField)`
-width: 50%;
-height:55px;
+  width: 50%;
+  height: 55px;
 
-element.style {
-    height:50px
-}
-.css-dpjnhs-MuiInputBase-root-MuiOutlinedInput-root {
+  element.style {
+    height: 50px;
+  }
+  .css-dpjnhs-MuiInputBase-root-MuiOutlinedInput-root {
+  }
 
-}
-
-.css-1sqnrkk-MuiInputBase-input-MuiOutlinedInput-input {
-font-size:1.5rem;
-height:55px;
-padding: 5px
-}
-`
+  .css-1sqnrkk-MuiInputBase-input-MuiOutlinedInput-input {
+    font-size: 1.5rem;
+    height: 55px;
+    padding: 5px;
+  }
+`;
 const StDescBox = styled(TextField)`
-width: 50%;
-.css-1sqnrkk-MuiInputBase-input-MuiOutlinedInput-input {
-font-size:1.5rem;
-padding: 5px
-}
-`
+  width: 50%;
+  .css-1sqnrkk-MuiInputBase-input-MuiOutlinedInput-input {
+    font-size: 1.5rem;
+    padding: 5px;
+  }
+`;
 
 const StTags = styled.div`
-
-.kkqTPl {
+  .kkqTPl {
     width: 50%;
     height: 55px;
     border-radius: 10px;
-    border: 1px solid #A0A0A0;
-    box-shadow: 0px 4px 5px rgba(0,0,0,0.1);
+    border: 1px solid #a0a0a0;
+    box-shadow: 0px 4px 5px rgba(0, 0, 0, 0.1);
     border: 1px solid;
-}
-`
-
+  }
+`;
 
 // const StCategorySelectBox = styled.div`
 /* display: flex; */
--- src/components/createMeeting/ThemeSelect.jsx
+++ src/components/createMeeting/ThemeSelect.jsx
@@ -1,48 +1,43 @@
-import React, { useState } from 'react';
-import styled from 'styled-components';
-import Button from '../../elements/Button';
-import { useSelector } from 'react-redux';
+import React, { useState } from "react";
+import styled from "styled-components";
+import Button from "../../elements/Button";
+import { useSelector } from "react-redux";
 
 const ThemeSelect = ({ setTheme }) => {
+  let [btnActive, setBtnActive] = useState("");
 
-    let [btnActive, setBtnActive] = useState("");
-    // const themeList = ['자전거', '등산', '힐링','바다'];
-    const themeList = useSelector((state) => state.themeSlice.themeList);
+  const themeList = useSelector((state) => state.themeSlice.themeList);
 
-    const toggleActive = (e) => {
+  const toggleActive = (e) => {
+    setTheme(e.target.value);
+    setBtnActive((prev) => {
+      return e.target.value;
+    });
+  };
 
-        setTheme(e.target.value)
-        setBtnActive((prev) => {
-            return e.target.value;
-        });
-    };
-
-    return (
-        <StThemeSelectBox>
-            {themeList.map((theme, index) => {
-                return (
-                    <StButton
-                        value={theme.themeName}
-                        size="small"
-                        variant={(theme.themeName === btnActive ? "primary" : "gray")}
-                        key={index}
-                        onClick={toggleActive}
-                        themeImage={theme.themeImage}
-
-                    >
-                        {theme.themeName}
-                    </StButton>
-                )
-            })}
-
-        </StThemeSelectBox>
-    )
-}
+  return (
+    <StThemeSelectBox>
+      {themeList.map((theme, index) => {
+        return (
+          <StButton
+            value={theme.themeName}
+            size="small"
+            variant={theme.themeName === btnActive ? "primary" : "gray"}
+            key={index}
+            onClick={toggleActive}
+            themeImage={theme.themeImage}
+          >
+            {theme.themeName}
+          </StButton>
+        );
+      })}
+    </StThemeSelectBox>
+  );
+};
 
 export default ThemeSelect;
 
 const StThemeSelectBox = styled.div`
-
   font-weight: bold;
   display: flex;
   flex-direction: row;
@@ -52,9 +47,8 @@ const StThemeSelectBox = styled.div`
 `;
 
 const StButton = styled(Button)`
-    margin-right: 20px;
-    width:"100px";
-    /* background-image:URL(${props => props.themeImage}); 
+  margin-right: 20px;
+  width: "100px";
+  /* background-image:URL(${(props) => props.themeImage}); 
     background-size: cover; */
-`
-
+`;
