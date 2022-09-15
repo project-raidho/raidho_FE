66a18ef7a6133a83597371e41b0c6424e280c0d6
YooJinRa <rarastudy@naver.com>
2022-09-15 20:43:54 +0900

    [FIX] 게시글 수정

--- src/components/updatePost/UpdatePostTags.jsx
+++ src/components/updatePost/UpdatePostTags.jsx
@@ -1,48 +1,72 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Input from "../../elements/Input";
 import styled from "styled-components";
 
-const UpdatePostTags = ({ tags, selectedTags, tagMassage }) => {
-  const [postTags, setPostTags] = useState(tags);
+const CreatePostTags = ({ tags, selectedTags, tagMassage }) => {
+  const [postTags, setPostTags] = useState([]);
+  const [tagValidationMsg, setTagValidationMsg] = useState("");
+  const [checkAlert, setCheckAlert] = useState(false);
+
+  useEffect(() => {
+    setPostTags(tags);
+  }, [tags]);
 
   // ::: 태그 삭제하기
-  // const removeTags = (indexToRemove) => {
-  //   setPostTags([...postTags.filter((_, index) => index !== indexToRemove)]);
-  // };
+  const removeTags = (indexToRemove) => {
+    setTagValidationMsg(`${postTags[indexToRemove]} 태그가 삭제되었습니다.`);
+    setCheckAlert(true);
+    setPostTags([...postTags.filter((_, index) => index !== indexToRemove)]);
+  };
 
   // ::: 태그 추가하기
   const addTags = (event) => {
     if (event.target.value !== "") {
-      setPostTags([...postTags, event.target.value]);
-      selectedTags([...postTags, event.target.value]);
+      const checkDuplicateTag = postTags.filter(
+        (tag) => tag !== event.target.value
+      );
+      setPostTags([...checkDuplicateTag, event.target.value]);
+      selectedTags([...checkDuplicateTag, event.target.value]);
       event.target.value = "";
+
+      if (checkDuplicateTag.length === postTags.length) {
+        setTagValidationMsg("태그가 성공적으로 입력되었습니다.");
+        setCheckAlert(false);
+      } else {
+        setTagValidationMsg("중복된 값이 입력되었습니다.");
+        setCheckAlert(true);
+      }
     }
   };
 
   return (
-    <StUpdatePostTagsWrap>
-      <ul id="tags">
-        {/* {postTags.map((tag, index) => (
-          <li key={index} className="tag">
-            <span className="tagTitle">{tag}</span>
-            <span className="tagCloseIcon" onClick={() => removeTags(index)}>
-              x
-            </span>
-          </li>
-        ))} */}
-      </ul>
-      <Input
-        type="text"
-        onKeyUp={(event) => (event.key === "Enter" ? addTags(event) : null)}
-        placeholder={tagMassage}
-      />
-    </StUpdatePostTagsWrap>
+    <>
+      <StCreatePostTagsWrap>
+        <ul id="tags">
+          {postTags.map((tag, index) => (
+            <li key={index} className="tag">
+              <span className="tagTitle">{tag}</span>
+              <span className="tagCloseIcon" onClick={() => removeTags(index)}>
+                x
+              </span>
+            </li>
+          ))}
+        </ul>
+        <Input
+          type="text"
+          onKeyUp={(event) => (event.key === "Enter" ? addTags(event) : null)}
+          placeholder={tagMassage}
+        />
+      </StCreatePostTagsWrap>
+      <StValidationMsg checkAlert={checkAlert}>
+        {tagValidationMsg}
+      </StValidationMsg>
+    </>
   );
 };
 
-export default UpdatePostTags;
+export default CreatePostTags;
 
-const StUpdatePostTagsWrap = styled.div`
+const StCreatePostTagsWrap = styled.div`
   display: flex;
   align-items: flex-start;
   flex-wrap: wrap;
@@ -104,4 +128,13 @@ const StUpdatePostTagsWrap = styled.div`
       cursor: pointer;
     }
   }
+`;
+
+const StValidationMsg = styled.p`
+  font-size: 1.1rem;
+  font-weight: 300;
+  font-style: italic;
+  color: ${(props) =>
+    props.checkAlert ? "var(--red-color)" : "var(--main-color)"};
+  margin-bottom: 1rem;
 `;
