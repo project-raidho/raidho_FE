e110a29a29fe335f1b8a9aa97e956821782fb582
YooJinRa <rarastudy@naver.com>
2022-09-22 02:00:03 +0900

    [FEAT] 게시글 등록 mutation 적용

--- src/components/createPost/CreatePostContainer.jsx
+++ src/components/createPost/CreatePostContainer.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-// import { useMutation, useQueryClient } from "react-query";
+import { useMutation, useQueryClient } from "react-query";
 import { authInstance } from "../../shared/api";
 import CreatePostImage from "./CreatePostImage";
 import CreatePostContent from "./CreatePostContent";
@@ -66,13 +66,13 @@ const CreatePostContainer = () => {
     }
   };
 
-  // const queryClient = useQueryClient();
-  // const { mutate } = useMutation(onCreatePost, {
-  //   onSuccess: (data) => {
-  //     console.log(data);
-  //     queryClient.invalidateQueries("postLists");
-  //   },
-  // });
+  const queryClient = useQueryClient();
+  const { mutate } = useMutation(onCreatePost, {
+    onSuccess: (data) => {
+      console.log(data);
+      queryClient.invalidateQueries("postLists");
+    },
+  });
 
   return (
     <StCreatePostContainerWrap>
@@ -105,7 +105,7 @@ const CreatePostContainer = () => {
         >
           취소
         </Button>
-        <Button size="squareTheme" variant="lineBlue" onClick={onCreatePost}>
+        <Button size="squareTheme" variant="lineBlue" onClick={mutate}>
           등록
         </Button>
       </StButtonWrap>
