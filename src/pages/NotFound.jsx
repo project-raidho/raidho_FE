20262c58e147bc3a49282ae98e5f712abf28d57a
YooJinRa <rarastudy@naver.com>
2022-09-27 02:33:26 +0900

    [FEAT] not found 페이지 스타일 작업

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,12 +1,46 @@
 import React from "react";
+import IconError from "../assets/iconError.svg";
 import styled from "styled-components";
 
 const NotFound = () => {
-  return <StNotFoundWrap>찾을 수 없는 페이지 입니다.</StNotFoundWrap>;
+  return (
+    <StNotFoundWrap>
+      <p>
+        <img src={IconError} alt="에러 페이지 입니다" />
+      </p>
+      <h3>NOT FOUND</h3>
+      <h4>찾을 수 없는 페이지입니다.</h4>
+    </StNotFoundWrap>
+  );
 };
 
 export default NotFound;
 
 const StNotFoundWrap = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
   width: 100%;
+
+  p {
+    width: 140px;
+    height: 140px;
+
+    img {
+      width: 100%;
+      height: 100%;
+      object-fit: contain;
+    }
+  }
+  h3 {
+    width: 180px;
+    font-size: 1.9rem;
+    text-align: center;
+  }
+  h4 {
+    width: 180px;
+    font-size: 1rem;
+    text-align: center;
+  }
 `;
