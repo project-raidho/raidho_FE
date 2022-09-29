3e7a69760007aa8911151f5dc12ebed1764ee6ea
YooJinRa <rarastudy@naver.com>
2022-09-29 23:56:47 +0900

    [FEAT] 모집리스트 찜하기 기능 구현

--- src/components/meetingList/MeetingListCard.jsx
+++ src/components/meetingList/MeetingListCard.jsx
@@ -8,6 +8,8 @@ import { useNavigate } from "react-router-dom";
 import AlertModal from "../../global/globalModal/AlertModal";
 import Potal from "../../global/globalModal/Potal";
 import LoginModal from "../login/LoginContainer";
+import MarkButton from "../../elements/MarkButton";
+
 // ::: 모집글 삭제 axios
 const onDeleteMeeting = async (meetingId) => {
   try {
@@ -116,8 +118,31 @@ const MeetingListCard = ({ meeting }) => {
     });
   };
 
+  // ::: 찜하기 버튼 기능 구현
+  const changeStar = async () => {
+    if (!meeting.isStarMine) {
+      await authInstance.post(`/api/meetingPostStar/${meeting.id}`);
+    } else {
+      await authInstance.delete(`/api/meetingPostStar/${meeting.id}`);
+    }
+  };
+
+  const { mutateStar } = useMutation(changeStar, {
+    onSuccess: () => {
+      queryClient.invalidateQueries("meetingList");
+    },
+    onError: () => {
+      setModalIcon("warning");
+      setAlertMsg("로그인 후 찜하기 버튼을 누를수 있습니다.");
+      setModalOn(true);
+    },
+  });
+
   return (
     <StMeetingListCardWrap>
+      <p className="markButton">
+        <MarkButton star={meeting.isStarMine} onClick={mutateStar} />
+      </p>
       <StMeetingCardUpDown>
         <StMeetingCardRow className="flexBetweenLayout">
           <p className="infoStatus">
@@ -262,6 +287,7 @@ const MeetingListCard = ({ meeting }) => {
 export default MeetingListCard;
 
 const StMeetingListCardWrap = styled.div`
+  position: relative;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
@@ -272,6 +298,14 @@ const StMeetingListCardWrap = styled.div`
   background-color: var(--subBg-color);
   border: 1px solid var(--gray-color);
   border-radius: 15px;
+
+  .markButton {
+    position: absolute;
+    width: 26px;
+
+    right: 6px;
+    top: -3px;
+  }
 
   h3 {
     font-size: 1.7rem;
@@ -492,6 +526,10 @@ const StMeetingCardRow = styled.div`
 
     .infoStatus {
       font-size: 1rem;
+
+      &.infoStatus:last-child {
+        margin-right: 20px;
+      }
     }
     .memberImageBox {
       width: 35px;
---
+++ src/elements/MarkButton.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import styled from "styled-components";
+import { RiBookmarkLine } from "react-icons/ri";
+import { RiBookmarkFill } from "react-icons/ri";
+
+const MarkButton = ({ star, onClick }) => {
+  return (
+    <StMark onClick={onClick}>
+      {star ? (
+        <RiBookmarkFill className="mark" />
+      ) : (
+        <RiBookmarkLine className="emptyMark" />
+      )}
+    </StMark>
+  );
+};
+
+export default MarkButton;
+
+const StMark = styled.span`
+  cursor: pointer;
+  svg {
+    width: 95%;
+    height: 95%;
+    object-fit: contain;
+  }
+  .emptyMark > path {
+    color: var(--gray-color);
+  }
+  .mark > path {
+    color: var(--red-color);
+  }
+`;
